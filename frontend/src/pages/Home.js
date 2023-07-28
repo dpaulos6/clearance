b8ccac52359d9065bd713c70b17e1c090f7fc919
dpaulos6 <itzframepvp@outlook.com>
2023-07-28 22:39:41 +0100

    Project Update
    Added final app logo.
    Added onClick function to the tabs with active class toggling.

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import { React, useState } from 'react'
 import './Home.scss'
 
 // Icons
@@ -7,36 +7,40 @@ import { BiLibrary } from 'react-icons/bi'
 import { FiDownload, FiShoppingCart } from 'react-icons/fi'
 import { LuSettings } from 'react-icons/lu'
 
+const menuItems = [
+  {name: 'Store', icon: FiShoppingCart},
+  {name: 'Recent', icon: BsClockHistory},
+  {name: 'Library', icon: BiLibrary},
+  {name: 'Downloads', icon: FiDownload},
+  {name: 'Friends', icon: BsPeople},
+  {name: 'Settings', icon:LuSettings},
+]
+
 export default function Home(){
+  const [active, setActive] = useState(null)
+
   return (
     <>
     <div className='flex flex-row'>
       <section id='menu-bar' className=''>
-        <div className='py-5'>
-          <img src='./eclipta_svg.png' alt="Eclipta Logo"/>
+        <div className='py-5 '>
+          <img src='./clearance_logo.svg' alt="Clearance Logo" className='clearance-logo'/>
         </div>
         <div className='controls'>
-          <a href='#' className='action'>
-            <FiShoppingCart className='icon'/>
-          </a>
-          <a href='#' className='action'>
-            <BsClockHistory className='icon'/>
-          </a>
-          <a href='#' className='action'>
-            <BiLibrary className='icon'/>
-          </a>
-          <a href='#' className='action'>
-            <FiDownload className='icon'/>
-          </a>
-          <a href='#' className='action'>
-            <BsPeople className='icon'/>
-          </a>
-          <a href='#' className='action'>
-            <LuSettings className='icon'/>
-          </a>
+          {menuItems.map((item) => {
+            return (
+              <a 
+                href='#'
+                onClick={() => setActive(item)}
+                className={`action ${active == item && 'active'}`}
+              >
+                <item.icon className='icon'/>
+              </a>
+            )
+          })}
         </div>
         <div className='py-5'>
-          <img src='./avatar_pfp.png' alt="User Avatar" className='rounded-full' />
+          <img src='./avatar_pfp.png' alt="User Avatar" className='avatar rounded-full' />
         </div>
       </section>
       <section className='w-full'>
